e0ea6a74aafd8c2df22f454c1e8f3a3887c79e31
biyongze <yongze.bi@centurygame.com>
2023-03-28 11:39:42 +0800

    comment: add comments

--- codec.go
+++ codec.go
@@ -6,11 +6,13 @@ import (
 
 const magicNumber byte = 0x7B
 
+// Codec is the interface that wraps the basic Marshal and Unmarshal methods.
 type Codec interface {
 	Marshal(v interface{}) ([]byte, error)
 	Unmarshal(data []byte, v interface{}) error
 }
 
+// Marshal ErrMarshalType is returned when the type of the value passed to Marshal is not supported.
 func Marshal(v interface{}) ([]byte, error) {
 	if _, ok := v.(*InternalCmd); !ok {
 		return nil, ErrMarshalType
@@ -22,6 +24,7 @@ func Marshal(v interface{}) ([]byte, error) {
 	return b, nil
 }
 
+// Unmarshal ErrUnmarshalNotInternalCmd is returned when the magicNumber verify failed.
 func Unmarshal(data []byte, v interface{}) error {
 	err := json.Unmarshal(data, v)
 	return err
